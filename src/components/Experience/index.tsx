ae395b9e56945b3009a22612c3f1f9ac8c6c3677
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2023-02-07 21:32:15 -0500

    [Feat]: Add technologies feature to projects cards on experience section (#5)
    * feat: config tailwinds and add navbar
    
    * feat: add font, add services folder, add home section
    
    * feat: Add contact component bar
    
    * feat: add Technologies components
    
    * feat: add experience section, and navbar scroll
    
    * feat: add styles changes and new url of cv
    
    * feat: add meta data fix styles
    
    * feat: add contact section, form services and validators
    
    * feat: change resume pdf url
    
    * feat: fix merge unsaved conflicts
    
    * feat: fix form field validation issue and styles
    
    * feat: add techs key to each project
    
    * Feat: add techs feature to experience projects card

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,11 +1,12 @@
 import exp from "../../services/experience.json";
 
-const Experience = ({experience}:any) => {
+const Experience = ({ experience }: any) => {
   const portfolioTeam = exp.teamwork.content;
   const portfolioSingle = exp.mywork.content;
 
   return (
-    <div ref={experience}
+    <div
+      ref={experience}
       className="py-[10vh] px-[20px] bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-full"
     >
       <div className="flex flex-col max-w-screen-lg p-4 mx-auto justify-center w-full h-full">
@@ -21,30 +22,85 @@ const Experience = ({experience}:any) => {
         </div>
 
         <div className="grid sm:grid-cols-2  gap-8 px-0 sm:px-0">
-          {portfolioTeam.map(({ id, title, description, img, liveDemo, BackendRepo, frontEndRepo }, index) => (
-            <div key={id} className="flex flex-col items-center justify-around shadow-md shadow-gray-600 rounded-lg">
-              <img
-                src={img}
-                alt={title}
-                className="rounded-md duration-200 w-full hover:scale-105"
-              />
-              <h3 className="text-lg font-bold py-[20px]">{title}</h3>
-              <div className="flex flex-col px-[30px] items-center justify-center">
-                <p className="text-justify">{description}</p>
+          {portfolioTeam.map(
+            (
+              {
+                id,
+                title,
+                description,
+                techs,
+                img,
+                liveDemo,
+                BackendRepo,
+                frontEndRepo,
+              },
+              index
+            ) => (
+              <div
+                key={id}
+                className="flex flex-col items-center justify-around shadow-md shadow-gray-600 rounded-lg"
+              >
+                <img
+                  src={img}
+                  alt={title}
+                  className="rounded-md duration-200 w-full hover:scale-105"
+                />
+                <h3 className="text-lg font-bold py-[20px]">{title}</h3>
+                <div className="flex flex-col px-[30px] items-center justify-center">
+                  <p className="text-justify">{description}</p>
+                  <h4 className="text-lg font-bold mt-[20px]">Technologies</h4>
+                  <div className="mt-[10px] gap-x-6  grid w-full grid-cols-3 md:grid-cols-4   ">
+                    {techs.map((tech, i) => (
+                      <ul className="flex items-center">
+                        <li>⭐</li>
+                        <li key={i} className="text-sm w-[100%]">
+                          {tech}
+                        </li>
+                      </ul>
+                    ))}
+                  </div>
+                </div>
+                <div className="flex items-center justify-center">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={liveDemo?.url}
+                    className={
+                      liveDemo?.disabled
+                        ? "hidden"
+                        : "w-1/3 font-bold cursor-pointer text-center w-1/3 px-0 py-3 m-4 duration-200 hover:scale-105"
+                    }
+                  >
+                    {exp.demoButton}
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={BackendRepo.url}
+                    className={
+                      BackendRepo?.disabled
+                        ? "hidden"
+                        : "w-1/3	font-bold cursor-pointer text-center px-0 py-3 m-4 duration-200 hover:scale-105"
+                    }
+                  >
+                    {exp.BackendRepoButton}
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={frontEndRepo.url}
+                    className={
+                      frontEndRepo?.disabled
+                        ? "hidden"
+                        : "w-1/3	font-bold cursor-pointer text-center px-0 py-3 m-4 duration-200 hover:scale-105"
+                    }
+                  >
+                    {exp.frontEndRepoButton}
+                  </a>
+                </div>
               </div>
-              <div className="flex items-center justify-center">
-                <a target="_blank" rel="noreferrer" href={liveDemo?.url} className={liveDemo?.disabled ? "hidden" : "w-1/3 font-bold cursor-pointer text-center w-1/3 px-0 py-3 m-4 duration-200 hover:scale-105"}>
-                  {exp.demoButton}
-                </a>
-                <a target="_blank" rel="noreferrer" href={BackendRepo.url} className={BackendRepo?.disabled ? "hidden" : "w-1/3	font-bold cursor-pointer text-center px-0 py-3 m-4 duration-200 hover:scale-105"}>
-                  {exp.BackendRepoButton}
-                </a>
-                <a target="_blank" rel="noreferrer" href={frontEndRepo.url} className={frontEndRepo?.disabled ? "hidden" : "w-1/3	font-bold cursor-pointer text-center px-0 py-3 m-4 duration-200 hover:scale-105"}>
-                  {exp.frontEndRepoButton}
-                </a>
-              </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
 
         <div className="flex pb-0 mt-[50px]">
@@ -52,31 +108,85 @@ const Experience = ({experience}:any) => {
         </div>
 
         <div className="grid sm:grid-cols-2  gap-8 px-0 sm:px-0">
-          {portfolioSingle.map(({ id, title, description, img, liveDemo, BackendRepo, frontEndRepo }, index) => (
-            <div key={id} className="flex flex-col items-center justify-around shadow-md shadow-gray-600 rounded-lg">
-              <img
-                src={img}
-                alt={title}
-                className="rounded-md duration-200 w-full hover:scale-105"
-              />
-              <h3 className="text-lg py-[20px] font-bold">{title}</h3>
-              <div className="flex flex-col px-[30px] items-center justify-center">
-
-                <p className="text-justify">{description}</p>
+          {portfolioSingle.map(
+            (
+              {
+                id,
+                title,
+                description,
+                techs,
+                img,
+                liveDemo,
+                BackendRepo,
+                frontEndRepo,
+              },
+              index
+            ) => (
+              <div
+                key={id}
+                className="flex flex-col items-center justify-around shadow-md shadow-gray-600 rounded-lg"
+              >
+                <img
+                  src={img}
+                  alt={title}
+                  className="rounded-md duration-200 w-full hover:scale-105"
+                />
+                <h3 className="text-lg py-[20px] font-bold">{title}</h3>
+                <div className="flex flex-col px-[30px] items-center justify-center">
+                  <p className="text-justify">{description}</p>
+                  <h4 className="text-lg font-bold">Technologies</h4>
+                  <div className="mt-[10px] gap-x-6  grid w-full grid-cols-3 md:grid-cols-4">
+                    {techs.map((tech, i) => (
+                      <ul className="flex items-center">
+                        <li>⭐</li>
+                        <li key={i} className="text-sm w-[100%]">
+                          {tech}
+                        </li>
+                      </ul>
+                    ))}
+                  </div>
+                </div>
+                <div className="flex flex-row items-center justify-center">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={liveDemo.url}
+                    className={
+                      liveDemo?.disabled
+                        ? "hidden"
+                        : "font-bold w-1/3	cursor-pointer text-center px-0 py-3 m-4 duration-200 hover:scale-105"
+                    }
+                  >
+                    {exp.demoButton}
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={BackendRepo.url}
+                    className={
+                      BackendRepo?.disabled
+                        ? "hidden"
+                        : "font-bold w-1/3	cursor-pointer text-center  px-0 py-3 m-4 duration-200 hover:scale-105"
+                    }
+                  >
+                    {exp.BackendRepoButton}
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={frontEndRepo.url}
+                    className={
+                      frontEndRepo?.disabled
+                        ? "hidden"
+                        : "font-bold w-1/3	cursor-pointer text-center px-0 py-3 m-4 duration-200 hover:scale-105"
+                    }
+                  >
+                    {exp.frontEndRepoButton}
+                  </a>
+                </div>
               </div>
-              <div className="flex flex-row items-center justify-center">
-                <a target="_blank" rel="noreferrer" href={liveDemo.url} className={liveDemo?.disabled ? "hidden" : "font-bold w-1/3	cursor-pointer text-center px-0 py-3 m-4 duration-200 hover:scale-105"}>
-                  {exp.demoButton}
-                </a>
-                <a target="_blank" rel="noreferrer" href={BackendRepo.url} className={BackendRepo?.disabled ? "hidden" : "font-bold w-1/3	cursor-pointer text-center  px-0 py-3 m-4 duration-200 hover:scale-105"}>
-                  {exp.BackendRepoButton}
-                </a>
-                <a target="_blank" rel="noreferrer" href={frontEndRepo.url} className={frontEndRepo?.disabled ? "hidden" : "font-bold w-1/3	cursor-pointer text-center px-0 py-3 m-4 duration-200 hover:scale-105"}>
-                  {exp.frontEndRepoButton}
-                </a>
-              </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
     </div>
