ac3ef348fdf6793ab7d581ba803031a1b61810e4
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2023-03-03 23:30:02 -0500

    [Fix]: Refactor code on navbar (#18)
    * feat: config tailwinds and add navbar
    
    * feat: add font, add services folder, add home section
    
    * feat: Add contact component bar
    
    * feat: add Technologies components
    
    * feat: add experience section, and navbar scroll
    
    * feat: add styles changes and new url of cv
    
    * feat: add meta data fix styles
    
    * feat: add contact section, form services and validators
    
    * feat: change resume pdf url
    
    * feat: fix merge unsaved conflicts
    
    * feat: fix form field validation issue and styles
    
    * feat: add techs key to each project
    
    * Feat: add techs feature to experience projects card
    
    * feat: start typing
    
    * feat: finish type on app.tsx component
    
    * feat: add types to all project
    
    * feat: fix contact bar, and types
    
    * feat: install trasnlation pac and init trasnlation
    
    * feat: add home translation, technologies, and missing packages
    
    * feat: finish translation content
    
    * feat: Add Resume in spanish
    
    * feat: finish translation feature
    
    * feat: contact bar merge conflict
    
    * Feat: Add language button change to navbar in mobile responsive
    
    * feat: close click outside drop lang
    
    * feat: Add About me Section
    
    * fix: styles about me
    
    * fix: contact styles
    
    * feat: add ux style change to language btn
    
    * feat: changes on about content
    
    * fix: styles on about me section md
    
    * fix: Refactor navbar code

--- src/services/sectionTypes/navbarTypes.ts
+++ src/services/sectionTypes/navbarTypes.ts
@@ -7,3 +7,28 @@ export type NavProps = {
   ) => void;
   arrayRef: React.RefObject<HTMLDivElement>[];
 };
+
+export type NavMobileProps = {
+  links: { id: number; link: string }[];
+  scrollToSection: (
+    elementRef: React.RefObject<HTMLDivElement>,
+    mobile: boolean
+  ) => void;
+  resumeURL: string;
+  arrayRef: React.RefObject<HTMLDivElement>[];
+  setLanguage: (lng: string) => void;
+};
+
+export type NavFullProps = {
+  refLang: React.RefObject<HTMLDivElement>;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  links: { id: number; link: string }[];
+  scrollToSection: (
+    elementRef: React.RefObject<HTMLDivElement>,
+    mobile: boolean
+  ) => void;
+  resumeURL: string;
+  arrayRef: React.RefObject<HTMLDivElement>[];
+  setLanguage: (lng: string) => void;
+};
